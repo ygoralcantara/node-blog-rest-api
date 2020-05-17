6520f644b9fef9739f849a7a365efe4950812e8a
ygoralcantara <ygoralcantaramedeiros@gmail.com>
2020-05-17 11:46:44 -0300

    Realizado ajustes

--- src/middlewares/errorHandler.middleware.ts
+++ src/middlewares/errorHandler.middleware.ts
@@ -1,20 +1,11 @@
 import { Request, Response, NextFunction } from 'express';
-import { httpStatus, AppError } from '@utils';
+import { AppError, jsend, httpStatus } from '@utils';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const errorHandler = (err: AppError, req: Request, res: Response, next: NextFunction): Response => {
-  if (err.status === httpStatus.NOT_FOUND) {
-    return res.status(httpStatus.NOT_FOUND).json({
-      status: 'fail',
-      message: err.message,
-      data: null,
-    });
-  }
+  const message = process.env.APP_ENVIROMENT === 'dev' ? err.message : 'Oops! Something went wrong';
 
-  return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
-    status: 'error',
-    message: process.env.APP_ENVIROMENT === 'dev' ? err.message : 'Oops! Something went wrong',
-  });
+  return res.status(httpStatus.INTERNAL_SERVER_ERROR).json(jsend.error(message));
 };
 
 export default errorHandler;
